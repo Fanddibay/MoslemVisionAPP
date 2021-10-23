aac22b8933013e9419510a573b884262f0cac3d5
Fanbayy <fandi.bayu110@gmail.com>
2021-10-23 21:01:44 +0700

    menambahkan javascript dan product page

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,46 @@
 function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  var mapProp = {
+    center: new google.maps.LatLng(51.508742, -0.12085),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+
+function toggleButtonShow() {
+  var moreTextLess = document.getElementById("moreTextLess");
+
+  var moreShow = document.getElementById("moreTextShow");
+
+  var buttonShow = document.getElementById("btnShowText");
+
+  if (moreTextLess.style.display === "none") {
+    moreShow.style.display = "none";
+
+    moreTextLess.style.display = "inline";
+
+    buttonShow.innerHTML = "Show more";
+  } else {
+    moreTextLess.style.display = "none";
+
+    moreShow.style.display = "inline";
+
+    buttonShow.innerHTML = "Lihat lebih sedikit";
+  }
+}
+
+// js product landing page
+function toggleBtnImg() {
+  var moreImg = document.getElementById("moreProduct");
+
+  var buttonImg = document.getElementById("moreBtnImg");
+
+  if (moreImg.style.display === "inline") {
+    moreImg.style.display = "none";
+
+    buttonImg.innerHTML = "Show more";
+  } else {
+    moreImg.style.display = "inline";
+
+    buttonImg.innerHTML = "Show less";
+  }
 }
